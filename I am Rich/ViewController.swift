4346f14c1d9cf27258ac91d1b8ab86fe254e9c95
Ali Mounim Rajabi <129315884+alimounim@users.noreply.github.com>
2025-10-02 12:16:54 -0500

    completed

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ali Mounim Rajabi on 10/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
